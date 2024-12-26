6d994cfa2893f36b1c9d545de2eb6b85f351bebf
MalaikaIrfan1 <malaikanagori1@gmail.com>
2024-12-26 18:09:29 +0500

    Add web application files

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask , render_template
+app = Flask(__name__)
+
+@app.route("/homepage")
+def index():
+    return render_template("homepage.html")
+
+@app.route("/about")
+def about():        
+    return render_template("about.html")
+
+@app.route("/memebership")
+def member():
+    return render_template("member.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
